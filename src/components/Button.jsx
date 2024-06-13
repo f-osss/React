dd48ed3430a642796e141a6b75cb2285957e1d1e
Faiza <salami.faiza15@gmail.com>
2024-06-13 15:15:42 -0500

    learned react basics

---
+++ src/components/Button.jsx
@@ -0,0 +1,6 @@
+const Button = (props) => {
+    // eslint-disable-next-line react/prop-types
+    return (<button>{props.text}</button>)
+};
+
+export default Button;
---
+++ src/components/Input.jsx
@@ -0,0 +1,6 @@
+const Input = (props) => {
+    // eslint-disable-next-line react/prop-types
+    return (<input type={props.type}/>)
+};
+
+export default Input;
